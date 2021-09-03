3fdfa37ee6ea64b014275d79d88e2a3fa3897ac7
gusti126 <gustidoang936@gmail.com>
2021-09-03 13:43:38 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\FrontController;
-use App\Http\Livewire\Admin\Artikel as AdminArtikel;
-use App\Http\Livewire\Artikel;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,34 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('pages.front.home');
-// });
-
-Route::get('/', [FrontController::class, 'home']);
-Route::get('/artikel/{slug}', [FrontController::class, 'detailArtikel'])->name('detail-artikel');
-Route::post('komentar/create/{id}', [FrontController::class, 'createKomen'])->name('create-komen');
-Route::get('/artikel', Artikel::class);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
-// admin
-Route::prefix('admin')->middleware(['admin', 'auth'])->group(function () {
-    Route::get('/artikel', AdminArtikel::class)->name('artikel');
-    Route::get('test', function () {
-        return view('layouts.admin');
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
-// Route::get('admin/artikel', function () {
-//     return view('layouts.admin');
-// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    mode: 'jit',
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
@@ -14,15 +12,14 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
-            backgroundImage: (theme) => ({
-                'imgHead': "url('/gambar/Rectangle 1.png')",
-                'coach': "url('/gambar/executive-coaching.jpg')",
-                })
-            },
-            minHeight:{
-                'highHeader': '500px'
-            }
+        },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
 };
